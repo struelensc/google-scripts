523b8aba1e828a192ad6026a738fd608b0becdaa
Courtney Struelens <43919547+struelensc@users.noreply.github.com>
2023-10-03 14:24:30 -0700

    successfully added all events objects to array

---
+++ scripts/updateCalendar.js
@@ -0,0 +1,50 @@
+function onOpen() {
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu("Sync to Calendar")
+    .addItem("Update Calendar", "updateCalendar")
+    .addSeparator()
+    .addSubMenu(
+      ui
+        .createMenu("Delete")
+        .addItem("Delete events from Calendar", "clearCalendar")
+    )
+    .addToUi();
+}
+
+function clearCalendar() {}
+
+function updateCalendar() {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadsheet.getActiveSheet();
+  var range = sheet.getDataRange();
+  var calendarId = spreadsheet.getRange("O1").getValue();
+  var eventCal = CalendarApp.getCalendarById(calendarId);
+
+  var eventsToSchedule = [];
+
+  var lastRow = range.getLastRow();
+  var r = 4; // Excluding headers
+
+  // Adding all the rows that have a name and date range.
+  while (r <= lastRow) {
+    var event = [];
+
+    var nameValue = range.getCell(r, 2).getValue();
+    var startDateValue = range.getCell(r, 4).getValue();
+    var endDateValue = range.getCell(r, 5).getValue();
+
+    if (nameValue != "" && startDateValue != "") {
+      event.push(nameValue);
+      event.push(startDateValue);
+    }
+
+    if (endDateValue != "") {
+      event.push(endDateValue);
+    }
+
+    eventsToSchedule.push(event);
+    r++;
+  }
+
+  Logger.log(eventsToSchedule);
+}
