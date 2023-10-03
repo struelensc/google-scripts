47471dad4869b5b12027a5315469cfcc43d05ef5
Courtney Struelens <43919547+struelensc@users.noreply.github.com>
2023-10-03 15:57:39 -0700

    wired deleting events

--- scripts/show-calender/clearCalendar.js
+++ scripts/show-calender/clearCalendar.js
@@ -1,6 +1,16 @@
 function clearCalendar() {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   var calendarId = spreadsheet.getRange("O1").getValue();
   var eventCal = CalendarApp.getCalendarById(calendarId);
 
-  eventCal.clear();
+  var fromDate = new Date(2000, 0, 0, 0, 0, 0);
+  var toDate = new Date(3000, 0, 0, 0, 0, 0);
+
+  var events = eventCal.getEvents(fromDate, toDate);
+
+  for (var i = 0; i < events.length; i++) {
+    var event = events[i];
+    Logger.log(event.getTitle());
+    event.deleteEvent();
+  }
 }
