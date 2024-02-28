22c538d280f455b34a9f6005039d152487dacea9
Courtney Struelens <43919547+struelensc@users.noreply.github.com>
2024-02-27 17:06:13 -0800

    added clear PO option

--- scripts/production-recaps/onOpen.js
+++ scripts/production-recaps/onOpen.js
@@ -2,5 +2,7 @@ function onOpen() {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu("Recap Options")
     .addItem("Create Recap", "getSKUData")
+    .addSeparator()
+    .addSubMenu(ui.createMenu("New PO").addItem("Clear current PO", "clearPO"))
     .addToUi();
 }
