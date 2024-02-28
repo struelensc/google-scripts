ba730e80914fb0962dd7dc6e895cd6aa0c246adf
Courtney Struelens <43919547+struelensc@users.noreply.github.com>
2024-02-27 17:05:20 -0800

    added ASN sheet to the clear function

--- scripts/production-recaps/clearPO.js
+++ scripts/production-recaps/clearPO.js
@@ -1,5 +1,7 @@
 function clearPO() {
   let ss = SpreadsheetApp.getActiveSpreadsheet();
   let poSheet = ss.getSheetByName("POST PO HERE");
+  let asnSheet = ss.getSheetByName("POST ASN HERE");
   poSheet.clear();
+  asnSheet.clear();
 }
