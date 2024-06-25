b927240df01f3a1657479c1c5362fc538322b67a
Courtney Struelens <43919547+struelensc@users.noreply.github.com>
2024-06-25 15:43:22 -0700

    updated clear PO to delete old recaps

--- scripts/production-recaps/clearPO.js
+++ scripts/production-recaps/clearPO.js
@@ -2,6 +2,10 @@ function clearPO() {
   let ss = SpreadsheetApp.getActiveSpreadsheet();
   let poSheet = ss.getSheetByName("POST PO HERE");
   let asnSheet = ss.getSheetByName("POST ASN HERE");
+  let oldRecapSheet = ss.getSheetByName("RECAP");
+  let oldBoxmakingSheet = ss.getSheetByName("BOXMAKING");
   poSheet.clear();
   asnSheet.clear();
+  ss.deleteSheet(oldRecapSheet);
+  ss.deleteSheet(oldBoxmakingSheet);
 }
